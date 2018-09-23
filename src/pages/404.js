c08c7be0696342a46a658617261f61cb84241acf
Jason Yeung <jsy9@sfu.ca>
2018-09-23 01:00:44 -0700

    add 404 page & styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className='p-404'>
+    <div className='o-container o-container--content u-relative'>
+      <div className='p-404__num'>
+        <img src='static/assets/404.svg'/>
+      </div>
+      <div className='c-label'>Error 404</div>
+      <h1>Page Not Found</h1>
+      <p className='u-mt-20'>
+        Uh oh! There seems to be nothing here!
+        <br></br><strong>Where you can go from here is to</strong>
+        <br></br><Link to='/' className='c-link c-link--alt'>Start at the beginning</Link>
+      </p>
+    </div>
   </div>
 )
 
