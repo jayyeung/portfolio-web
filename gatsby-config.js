af279ec3d1d91604293cfd65294355835218b1ed
Jason Yeung <jsy9@sfu.ca>
2018-09-24 00:29:38 -0700

    add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,11 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    'gatsby-plugin-netlify-cms',
+    {
+      resolve: 'gatsby-plugin-layout',
+      options: { component: require.resolve('./src/components/layout.js') }
+    }
   ],
 }
